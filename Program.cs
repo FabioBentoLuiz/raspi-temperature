c002da0680119b0779d97afaa945ee7960b6af49
Fabio Bento Luiz <bento_85@hotmail.com>
2021-09-26 13:41:50 +0200

    initial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace raspi_temperature
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

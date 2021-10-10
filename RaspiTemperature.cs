14ee2a6f987a020c0d8ef746e97e6f2d98f1f647
Fabio Bento Luiz <bento_85@hotmail.com>
2021-10-10 13:55:10 +0200

    refactoring

--- RaspiTemperature.cs
+++ RaspiTemperature.cs
@@ -15,16 +15,27 @@ namespace raspi_temperature
 
         public RaspiTemperature()
         {
-            var connectionString = Environment.GetEnvironmentVariable("RASPI_01_CS");
-            if(String.IsNullOrEmpty(connectionString)) {
-                throw new InvalidOperationException("Environment variable RASPI_01_CS is not set.");
-            }
-            deviceClient = DeviceClient.CreateFromConnectionString(connectionString, TransportType.Mqtt);
+            deviceClient = GetAzureIotHubClient();
+        }
+
+        private DeviceClient GetAzureIotHubClient() {
+            var connectionString = GetConnectionString();
+            var deviceClient = DeviceClient.CreateFromConnectionString(connectionString, TransportType.Mqtt);
             deviceClient.SetConnectionStatusChangesHandler((status, reason) =>
             {
                 Console.WriteLine($"Connection status change registered - status={status}, reason={reason}.");
             });
-            
+
+            return deviceClient;
+        }
+
+        private string GetConnectionString() {
+            var connectionString = Environment.GetEnvironmentVariable("RASPI_01_CS");
+            if(String.IsNullOrEmpty(connectionString)) {
+                throw new InvalidOperationException("Environment variable RASPI_01_CS is not set.");
+            }
+
+            return connectionString;
         }
 
         public async Task Run()
@@ -36,28 +47,31 @@ namespace raspi_temperature
             using (var device = I2cDevice.Create(settings))
             {
                 while(true) {
-                    var analogVal = device.ReadByte();
-
-                    var rawTemperature = Convert.ToDouble(analogVal);
-                    var Vr = 5 * (rawTemperature) / 255;
-                    var Rt = 10000 * Vr / (5 - Vr);
-                    var temp = 1 / (((Math.Log(Rt / 10000)) / 3950) + (1 / (273.15 + 25)));
-                    temp = temp - 273.15;
+                    var temperatureCelsius = ParseToCelsius(device.ReadByte());
+                    Console.WriteLine($"Current temperature: {temperatureCelsius} C");
                     
-                    Console.WriteLine("final temp: " + temp);
-
-                    var temperatureData = new TemperatureData { Id = "1", TemperatureCelsius = temp};
-
+                    var msg = CreateMessage(temperatureCelsius);
+                    await deviceClient.SendEventAsync(msg);
                     
-
-                    var messageString  = JsonConvert.SerializeObject(temperatureData);
-                    var message = new Message(Encoding.ASCII.GetBytes(messageString));
-
-                    await deviceClient.SendEventAsync(message);
-                    
-                    Task.Delay(2000).Wait();
+                    await Task.Delay(2000);
                 }
             }
+        }
+
+        private Message CreateMessage(double temperatureCelsius) {
+            var temperatureData = new TemperatureData { Id = "1", TemperatureCelsius = temperatureCelsius};
+            var messageString  = JsonConvert.SerializeObject(temperatureData);
+            return new Message(Encoding.ASCII.GetBytes(messageString));
+        }
+
+        private double ParseToCelsius(byte analogicTemperature) {
+            var rawTemperature = Convert.ToDouble(analogicTemperature);
+            var Vr = 5 * (rawTemperature) / 255;
+            var Rt = 10000 * Vr / (5 - Vr);
+            var temp = 1 / (((Math.Log(Rt / 10000)) / 3950) + (1 / (273.15 + 25)));
+            temp = temp - 273.15;
+
+            return temp;
         }
     }
 }
